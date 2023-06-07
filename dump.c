05dfff038a8614edc44605eb78de05139807b4d8
Ramon Fried <ramon@neureality.ai>
2023-06-07 11:55:24 +0300

    dump: Add suport for raw ascii dump

--- dump.c
+++ dump.c
@@ -21,6 +21,7 @@ static void do_dump_help(FILE *output)
 	fprintf(output, "Options:\n");
 	fprintf(output, " -m, --mem-dev\t\t memory device to use (default is /dev/mem)\n");
 	fprintf(output, " -C, --canonical\t canonical hex+ASCII display\n");
+	fprintf(output, " -a, --ascii\t ASCII display\n");
 	fprintf(output, " -v, --no-squeezing\t output identical lines\n\n");
 	fprintf(output, " -h, --help\t\t Display this help screen\n");
 	fprintf(output, "Arguments:\n");
@@ -36,6 +37,7 @@ int do_dump(int argc, char **argv)
 	int fd;
 	int page_count;
 	int canonical = 0;
+	int ascii = 0;
 	int squeeze = 1;
 	off_t target;
 	off_t size;
@@ -49,13 +51,14 @@ int do_dump(int argc, char **argv)
 			{"mem-dev", required_argument, 0, 'm'},
 		    {"canonical", no_argument, 0, 'C'},
 		    {"no-squeezing", no_argument, 0, 'v'},
+		    {"ascii", no_argument, 0, 'a'},
 		    {"help", no_argument, 0, 'h'},
 		    {0, 0, 0, 0}
 		};
 		// clang-format on
 		int option_index = 0;
 
-		c = getopt_long(argc, argv, "m:Cvh", long_options, &option_index);
+		c = getopt_long(argc, argv, "m:Cvha", long_options, &option_index);
 
 		/* Detect the end of the options. */
 		if (c == -1)
@@ -67,6 +70,9 @@ int do_dump(int argc, char **argv)
 			break;
 		case 'C':
 			canonical = 1;
+			break;
+		case 'a':
+			ascii = 1;
 			break;
 		case 'v':
 			squeeze = 0;
@@ -100,6 +106,10 @@ int do_dump(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
+	if (canonical && ascii) {
+		fprintf(stderr, "Ascii & Canonical are mutual exclusive options\n");
+		return EXIT_FAILURE;
+	}
 	/* Get address */
 
 	page_size = sysconf(_SC_PAGESIZE);
@@ -136,34 +146,46 @@ int do_dump(int argc, char **argv)
 	while (size > 0) {
 		int i;
 
-		if (squeeze && (!first) && ((size - 16) >= 16)) {
-			if (memcmp(virt_addr, virt_addr + 16, 16) == 0) {
-				if (!in_squeeze) {
-					printf("*\n");
-					in_squeeze = true;
+		if (!ascii) {
+				if (squeeze && (!first) && ((size - 16) >= 16)) {
+					if (memcmp(virt_addr, virt_addr + 16, 16) == 0) {
+						if (!in_squeeze) {
+							printf("*\n");
+							in_squeeze = true;
+						}
+						goto proceed;
+					} else {
+						in_squeeze = false;
+					}
 				}
-				goto proceed;
-			} else {
-				in_squeeze = false;
-			}
-		}
 
-		if (target >= ULONG_MAX)
-			printf("0x%.16" PRIx64 "  ", target);
-		else
-			printf("0x%.8" PRIx64 "  ", target);
-		for (i = 0; i < 16; i++) {
-			if (i == 8)
-				putchar(' ');
-			printf("%02x ", *(volatile uint8_t *)(virt_addr + i));
-		}
-		if (canonical) {
-			printf(" |");
-			for (i = 0; i < 16 && (size - i) > 0; i++) {
-				char c = *(volatile uint8_t *)(virt_addr + i);
-				printf("%c", isgraph(c) ? c : '.');
-			}
-			printf("|");
+				if (target >= ULONG_MAX)
+					printf("0x%.16" PRIx64 "  ", target);
+				else
+					printf("0x%.8" PRIx64 "  ", target);
+				for (i = 0; i < 16; i++) {
+					if (i == 8)
+						putchar(' ');
+					printf("%02x ", *(volatile uint8_t *)(virt_addr + i));
+				}
+				if (canonical) {
+					printf(" |");
+					for (i = 0; i < 16 && (size - i) > 0; i++) {
+						char c = *(volatile uint8_t *)(virt_addr + i);
+						printf("%c", isgraph(c) ? c : '.');
+					}
+					printf("|");
+				}
+		} else {
+				for (i = 0; i < size; i++) {
+					char c = *(volatile uint8_t *)(virt_addr + i);
+					if (isascii(c))
+							putchar(c);
+					else
+							putchar('.');
+				}
+				putchar('\n');
+				goto exit1;
 		}
 		putchar('\n');
 proceed:
@@ -172,6 +194,7 @@ proceed:
 		target += 16;
 		first = false;
 	}
+exit1:
 
 	if (munmap(map_base, mapped_size) == -1) {
 		perror("Can't unmap memory");
